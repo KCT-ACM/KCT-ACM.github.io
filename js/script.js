99c868588855f47fda522f1e70e6055b40809ed6
Jayashakthi28 <55621768+Jayashakthi28@users.noreply.github.com>
2021-02-17 12:09:30 +0530

    Added loading animation

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ const nav=document.querySelector('nav');
 toggleCollapse.onclick=()=>{
     nav.classList.toggle("collapse");
 }
+/*********************PAGE LOADER******************/
+var animation=bodymovin.loadAnimation({
+	container:document.getElementById('page-load'),
+	loop:true,
+	autoplay:true,
+	path:'./js/data.json'
+})
+window.onload = function () {
+    window.setTimeout(function () {
+      $("#body").addClass("body");
+    }, 1000);
+  };
+  $(window).on("load", function () {
+    $("#page-load").delay(1000).fadeOut("slow");
+  });
