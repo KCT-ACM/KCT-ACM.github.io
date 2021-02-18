04313b957f920f1f2ac73f239c8b9cc37891f542
Jayashakthi28 <vishnu28.js@gmail.com>
2021-02-18 12:50:57 +0530

    Made navbarfully responsive fixed some bugs added ease animation

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ const toggleCollapse=document.querySelector('nav .ham-burger');
 const nav=document.querySelector('nav');
 toggleCollapse.onclick=()=>{
     nav.classList.toggle("collapse");
+    if($('.ham').hasClass("fa-bars")){
+      $('.ham').addClass("fa-times").removeClass("fa-bars")
+    }
+    else{
+      $('.ham').addClass("fa-bars").removeClass("fa-times")
+    }
 }
 /*********************PAGE LOADER******************/
 var animation=bodymovin.loadAnimation({
