052f5b2cb7875845cc8b736a03ba91a38f538709
Jayashakthi28 <55621768+Jayashakthi28@users.noreply.github.com>
2021-02-22 12:15:34 +0530

    Added about us section , Animated its background ,Added smooth scrolling

--- js/script.js
+++ js/script.js
@@ -9,6 +9,20 @@ toggleCollapse.onclick=()=>{
       $('.ham').addClass("fa-bars").removeClass("fa-times")
     }
 }
+const speed = 1000;
+
+    $('a[href*="#"]')
+      .filter((i, a) => a.getAttribute('href').startsWith('#') || a.href.startsWith(`${location.href}#`))
+      .unbind('click.smoothScroll')
+      .bind('click.smoothScroll', event => {
+        const targetId = event.currentTarget.getAttribute('href').split('#')[1];
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+          event.preventDefault();
+          $('html, body').animate({ scrollTop: $(targetElement).offset().top }, speed);
+        }
+      });
 /*********************PAGE LOADER******************/
 var animation=bodymovin.loadAnimation({
 	container:document.getElementById('page-load'),
@@ -50,3 +64,4 @@ window.onload = function () {
         }
     }
 })
+/********************************************************************** */
