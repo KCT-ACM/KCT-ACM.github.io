3ca06f29fcb6ed8f87ae62d65f60127a1f5a7f5b
Jayashakthi28 <55621768+Jayashakthi28@users.noreply.github.com>
2021-02-23 08:56:07 +0530

    Added our team section

---
+++ js/bubble code.js
@@ -0,0 +1,60 @@
+particlesJS("BeehiveID",
+
+    {
+        "particles": {
+            "number": {
+                "value": 50
+            },
+            "color": {
+            "value": ['#fff', '#443366','#ff9382','#22ff99','#3388ff' ]
+            },
+            "opacity": {
+            "value": 0.8,
+            "random": true,
+            "anim": {
+                "enable": true,
+                "speed": 2,
+                "opacity_min": 0.3,
+                "sync": false
+            }
+        },
+
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0.5,
+                    "color": '#fff'
+                }
+
+            },
+            "size": {
+                "value": 60,
+                "random": true
+            },
+            "line_linked": {
+                "enable": false
+            },
+            "move": {
+                "enable": true,
+                "speed": 3,
+                "direction": "top",
+                "straight": false
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": false
+                }
+            },
+            "modes": {
+                "push": {
+                    "particles_nb": 12
+                }
+            }
+        }
+    }
+
+
+);
